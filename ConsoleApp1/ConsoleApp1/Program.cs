dd83acff13c222fa4ace281059137997682ee741
markforest <toxerius7@gmail.com>
2018-11-11 09:52:02 +0200

    Добавил анимацию

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,6 +11,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            someFunc2();
+        }
+
+        private static void someFunc2()
+        {
+            foreach (var item in "Hello Git")
+            {
+                Console.WriteLine(item);
+                Thread.Sleep(500);
+                Console.Beep();
+            }
         }
     }
 }
